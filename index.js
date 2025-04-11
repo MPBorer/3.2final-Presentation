697b7d621ec3e748b00a2dc9ec62417d6cd9a290
Michael <mpborer@gmail.com>
2025-04-11 09:44:35 -0600

    2nd

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function main() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users")
-    const usersData = users.json();
-    console.log(
-      usersData.map(
-        (user) => `<div class="user-card">
-    <div class="user-card__container">
-        <h3>User's Name</h4>
-        <p><b>Email:</b> email@email.com</p>
-        <p><b>Phone:</b> 0000000000</p>
-        <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-    </div>
-    </div>`
-     )
-    );
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+    const userListEL = document.querySelector(`.user-list`)
+   console.log(usersData)
+    userListEL.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
+
 main();
+
+function userHtml(user) {
+   return `<div class="user-card">
+    <div class="user-card__container">
+       <h3>${user.name}</h4>
+         <p><b>Email:</b> ${user.email}</p>
+         <p><b>Phone:</b> ${user.phone}</p>
+         <p><b>Website:</b> <a href="https://${user.website}"
+          target="_blank">${user.website}</a></p>
+    </div>
+</div>`
+}
